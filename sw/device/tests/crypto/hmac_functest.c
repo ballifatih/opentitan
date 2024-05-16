0138e0c12532dd07b8f674246c632a4e2e036baf
Fatih Balli <fatihballi@google.com>
2024-05-16 12:41:52 +0000

    [hmac, testing] Add
    This PR adds a big testing harness for HMAC.
    
    Namely, it brings:
    * A py script to generate a HMAC/SHA2 vector for varying sizes (using Crypto.Hash lib)
    * Another py script to determine random parameters and run the prev script for to obtain 20 vecs
    * tpl/set scripts to conver generated hjson headers to C headers
    * hmac_functest to drive all variants of SHA2/HMAC functions from the generated vectors.
    
    Signed-off-by: Fatih Balli <fatihballi@google.com>

---
+++ sw/device/tests/crypto/hmac_functest.c
@@ -0,0 +1,70 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/crypto/drivers/entropy.h"
+#include "sw/device/lib/crypto/drivers/hmac.h"
+#include "sw/device/lib/crypto/impl/integrity.h"
+#include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/crypto/include/hash.h"
+#include "sw/device/lib/crypto/include/kdf.h"
+#include "sw/device/lib/crypto/include/mac.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+
+// The autogen rule that creates this header creates it in a directory named
+// after the rule, then manipulates the include path in the
+// cc_compilation_context to include that directory, so the compiler will find
+// the version of this file matching the Bazel rule under test.
+#include "hmac_testvectors.h"
+
+// Module ID for status codes.
+#define MODULE_ID MAKE_MODULE_ID('h', 'm', 'a')
+
+// Global pointer to the current test vector.
+static hmac_test_vector_t *current_test_vector = NULL;
+
+/**
+ * Run the test pointed to by `current_test_vector`.
+ */
+static status_t run_test_vector(void) {
+
+  // Populate `checksum` and `config.security_level` fields.
+  current_test_vector->key.checksum = 
+      integrity_blinded_checksum(&current_test_vector->key);
+
+  // TODO handle larger digest sizes.
+  uint32_t act_tag[8];
+  otcrypto_word32_buf_t tag_buf = {
+      .data = act_tag,
+      .len = ARRAYSIZE(act_tag),
+  };
+  otcrypto_hash_digest_t hash_digest = {
+    .mode = kOtcryptoHashModeSha256,
+    .data = act_tag,
+    .len = 8,
+  };
+  if(current_test_vector->test_operation == kHmacTestOperationSha256) {
+    TRY(otcrypto_hash(current_test_vector->message, hash_digest));
+  } else if (current_test_vector->test_operation == kHmacTestOperationHmacSha256) {
+    TRY(otcrypto_hmac(&current_test_vector->key, current_test_vector->message, tag_buf));
+  }
+  TRY_CHECK_ARRAYS_EQ(act_tag, current_test_vector->digest.data, 8);    
+  return OTCRYPTO_OK;
+}
+
+OTTF_DEFINE_TEST_CONFIG();
+bool test_main(void) {
+  LOG_INFO("Testing cryptolib HMAC driver.");
+  CHECK_STATUS_OK(entropy_complex_init());
+  status_t test_result = OK_STATUS();
+  for (size_t i = 0; i < ARRAYSIZE(kHmacTestVectors); i++) {
+    current_test_vector = &kHmacTestVectors[i];
+    LOG_INFO("Running test %d of %d, test vector identifier: %s", i + 1,
+             ARRAYSIZE(kHmacTestVectors),
+             current_test_vector->vector_identifier);
+    EXECUTE_TEST(test_result, run_test_vector);
+  }
+  return status_ok(test_result);
+}
---
+++ sw/device/tests/crypto/hmac_gen_random_testvectors.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import random
+import sys
+
+import hjson
+import hmac_gen_single_testvector
+'''
+Generate multiple test vectos
+'''
+
+
+def gen_random_test(idx):
+    random_instance = random.Random(idx)
+    operation = random_instance.choice(["SHA256", "HMAC256"])
+    input_msg_len = 8 * random_instance.randint(0, 400)
+    # Ensure that the key length is larger or equal to security parameter.
+    # Also, KMAC HWIP only supports a discrete set of key lengths.
+    if operation in ["HMAC256"]:
+        key_len = 256
+    else:
+        key_len = 0
+
+    return hmac_gen_single_testvector.gen_random_test(idx, operation, input_msg_len, key_len)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n',
+                        type=int,
+                        help='Number of random test vectors to generate.')
+    parser.add_argument('outfile',
+                        metavar='FILE',
+                        type=argparse.FileType('w'),
+                        help='Write output to this file.')
+
+    args = parser.parse_args()
+
+    testvecs = []
+    for i in range(args.n):
+        testvecs.append(gen_random_test(i))
+
+    hjson.dump(testvecs, args.outfile)
+    args.outfile.close()
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
